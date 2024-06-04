d0ad3cde4917a265b3c74412b70b6f1e77b4cb0b
SoniiPP <sonypoudel30@gmail.com>
2024-06-05 02:33:29 +1000

    added colums on models and some api routes

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,13 +1,37 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../../config/connection');
+const Product = require('./Product');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
+  // define columns
   {
-    // define columns
+    id: {
+      types: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_id: {
+      types: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      types: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
+
+
   {
     sequelize,
     timestamps: false,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,29 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name'],
+        through: ProductTag,
+        as: 'product_tags'
+      }
+
+    ]
+  }).then(dbProductData => {
+    if (!dbProductData) {
+      res.status(400).json({ message: 'No products found' });
+    }
+    res.send(dbProductData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 // get one product
@@ -57,26 +80,26 @@ router.put('/:id', (req, res) => {
   })
     .then((product) => {
       if (req.body.tagIds && req.body.tagIds.length) {
-        
+
         ProductTag.findAll({
           where: { product_id: req.params.id }
         }).then((productTags) => {
           // create filtered list of new tag_ids
           const productTagIds = productTags.map(({ tag_id }) => tag_id);
           const newProductTags = req.body.tagIds
-          .filter((tag_id) => !productTagIds.includes(tag_id))
-          .map((tag_id) => {
-            return {
-              product_id: req.params.id,
-              tag_id,
-            };
-          });
+            .filter((tag_id) => !productTagIds.includes(tag_id))
+            .map((tag_id) => {
+              return {
+                product_id: req.params.id,
+                tag_id,
+              };
+            });
 
-            // figure out which ones to remove
+          // figure out which ones to remove
           const productTagsToRemove = productTags
-          .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-          .map(({ id }) => id);
-                  // run both actions
+            .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+            .map(({ id }) => id);
+          // run both actions
           return Promise.all([
             ProductTag.destroy({ where: { id: productTagsToRemove } }),
             ProductTag.bulkCreate(newProductTags),
